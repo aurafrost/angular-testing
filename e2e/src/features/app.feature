22a37dff765441ed3e1885f093a43073dca16835
jimmytran <jt963369@gmail.com>
2019-04-02 09:30:17 -0400

    refactoring

--- e2e/src/features/app.feature
+++ e2e/src/features/app.feature
@@ -1,7 +1,17 @@
 Feature: Go to the home
-  Display the title
+  Display the title and other home page actions
 
   Scenario: Home Page
     Given I am on the home page
     When I do nothing
     Then I should see the title
+
+  Scenario Outline: Home Page
+    Given I am waiting on the home page
+    When I change the name to <newName>
+    Then I should see the name change
+
+  Examples:
+    | newName |
+    | "Bill" |
+    | "Bob" |
--- e2e/src/steps/app.steps.ts
+++ e2e/src/steps/app.steps.ts
@@ -9,12 +9,28 @@ Before(() => {
   page = new AppPage();
 });
 
-Given(/^I am on the home page$/, async () => {
+
+//Scenario 1
+Given("I am on the home page", async () => {
   await page.navigateTo();
 });
 
-When(/^I do nothing$/, () => {});
+When("I do nothing", () => {});
 
-Then(/^I should see the title$/, async () => {
+Then("I should see the title", async () => {
   expect(await page.getTitleText()).to.equal('Welcome to angular-testing!');
+});
+
+
+//Scenario 2
+Given("I am waiting on the home page", async () => {
+  await page.navigateTo();
+});
+
+When("I change the name to {string}", (newName) => {
+  page.changeInput(newName);
+});
+
+Then("I should see the name change", async () => {
+  expect(await page.getInput()).not.equal("");
 });
